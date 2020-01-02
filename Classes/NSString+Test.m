ae52a9f3938c9729fcc03882d47981e69e93a84f
hanyang <>
2020-01-02 10:30:51 +0800

    第一次提交

---
+++ Classes/NSString+Test.m
@@ -0,0 +1,20 @@
+//
+//  NSString+Test.m
+//  pod_test_Example
+//
+//  Created by ios-2 on 2020/1/2.
+//  Copyright © 2020 hanyang. All rights reserved.
+//
+
+#import "NSString+Test.h"
+
+#import <AppKit/AppKit.h>
+
+
+@implementation NSString (Test)
+
+- (void)test{
+	NSLog(@"只是一个测试");
+}
+
+@end
